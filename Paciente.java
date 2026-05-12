43108b8613f54611647ac14eecdf22793e45848b
A4DREK <yael.adrian640305@gmail.com>
2026-05-12 10:57:17 -0600

    Agregado del método compareTo y toString

--- Paciente.java
+++ Paciente.java
@@ -34,8 +34,21 @@ public class Paciente implements Comparable<Paciente> {
 
     @Override
     public int compareTo(Paciente compara){
+
+        if(this.prioridad > compara.prioridad){
+            return 1;
+        }else if(this.prioridad < compara.prioridad){
+            return -1;
+        }
         return 0;
+
     }
     
+    @Override
+    public String toString(){
+        return "|| Paciente: " + this.nombre + "|| \n"
+                + "|| Enfermedad: " + this.enfermedad + "|| \n"
+                + "|| Prioridad: " + this.prioridad + "|| \n"; 
+    }
 
 }
